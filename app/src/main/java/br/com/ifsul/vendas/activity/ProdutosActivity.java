50a3602fe2c9eee14f13265f6eb24c0bf28b1268
LaurenAlmeida <lauren_goncalves17@hotmail.com>
2019-03-29 17:12:30 -0300

    menu a terminar

--- app/src/main/java/br/com/ifsul/vendas/activity/ProdutosActivity.java
+++ app/src/main/java/br/com/ifsul/vendas/activity/ProdutosActivity.java
@@ -3,7 +3,9 @@ package br.com.ifsul.vendas.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
 import android.widget.ListView;
+import android.widget.SearchView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -22,12 +24,15 @@ public class ProdutosActivity extends AppCompatActivity {
 
     private static final String TAG = "produtosactivity";
     private ListView lvProdutos;
+    private List<Produto> produtos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_produtos);
+
         lvProdutos = findViewById(R.id.lv_produtos);
+        lvProdutos.setOnItemClickListener();
 
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -43,6 +48,7 @@ public class ProdutosActivity extends AppCompatActivity {
                 List<Produto> produtos = new ArrayList<>();
                 for(DataSnapshot ds : dataSnapshot.getChildren()){
                     Produto produto = ds.getValue(Produto.class);
+                    produto.setKey(ds.getKey());
                     produtos.add(produto);
                 }
 
@@ -56,5 +62,29 @@ public class ProdutosActivity extends AppCompatActivity {
                 Log.w(TAG, "Failed to read value.", error.toException());
             }
         });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_activity_produtos,menu);
+        SearchView searchView = (SearchView) menu.findItem.R.id.menuItem_pesquisar).getActionView();
+        searchView = setQueryHint(getString(R.string.hint_nome_searchview));
+        searchView = setQueryTextListener(new SearchView(setOnQueryTextListener(){
+
+            public boolean onQueryTextSubmit(String query){
+                return true;
+            }
+
+            @Override
+            public void setOnQueryTextChange(String newText) {
+              List<Produto> produtosTemp = new ArrayList<>();
+              for (Produto produto : produtos){
+                  if (produto.getNome().contains(newText)){
+                      produtosTemp.add(produto);
+                  }
+              }
+
+            }
+        });
+        return true;
     }
 }
