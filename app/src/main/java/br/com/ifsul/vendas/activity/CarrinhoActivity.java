4a4b0dfe5fdfe9f79e29dd10632cf036eb7205f2
LaurenAlmeida <39421295+laurenalmeida@users.noreply.github.com>
2019-04-24 15:37:47 -0300

    carrinho

--- app/src/main/java/br/com/ifsul/vendas/activity/CarrinhoActivity.java
+++ app/src/main/java/br/com/ifsul/vendas/activity/CarrinhoActivity.java
@@ -3,6 +3,10 @@ package br.com.ifsul.vendas.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.FirebaseDatabase;
 
 import br.com.ifsul.vendas.R;
 import br.com.ifsul.vendas.adapter.CarrinhoAdapter;
@@ -10,14 +14,28 @@ import br.com.ifsul.vendas.setup.AppSetup;
 
 public class CarrinhoActivity extends AppCompatActivity {
 
-    private ListView lv_carrinho;
+    private static final String TAG = "carrinhoActivity";
+    private ListView lvCarrinhos;
+    private TextView tvTotalPedidoCarrinho;
+    private Double totalPedido;
+    private FirebaseDatabase database;
+    private TextView tvClienteCarrinho;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carrinho);
-        lv_carrinho = findViewById(R.id.lv_carrinho);
-        lv_carrinho.setAdapter(new CarrinhoAdapter(CarrinhoActivity.this, AppSetup.carrinho));
+        ListView lv_carrinho = findViewById(R.id.lv_carrinho);
+
+        tvTotalPedidoCarrinho =findViewById(R.id.tvTotalPedidoCarrinho);
+        tvClienteCarrinho = findViewById(R.id.tvClienteCarrinho);
+
+        tvClienteCarrinho.setText(AppSetup.cliente.getNome() + " " + AppSetup.cliente.getSobrenome());
+        database = FirebaseDatabase.getInstance();
+
+
 
 
     }
